c698b2a6d27cb03a6e3d8eebcc8159ee1b55a864
M. Algarni <m.t.algarni@gmail.com>
2017-05-22 11:45:12 +0300

    console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from "@angular/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  cities: any[];
+  url: string = 'http://78.93.66.138/MadarMobileAppServicesMirror/WebServices/MadarWebServicesMirror.asmx/GetAllCities';
+
+  constructor(
+    private http: Http
+  ) { }
+
+  ngOnInit() {
+    this.http.get(this.url)
+      .subscribe(res => {
+          this.cities = res.json();
+          console.log(this.cities);
+      });
+  }
 }
